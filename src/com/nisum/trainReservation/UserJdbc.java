60fa638c70f1675eb324029413776e6cf8dbeb79
na9z8xt <nandigam_srinivas@gap.com>
2017-12-20 11:51:59 -0800

    Updated JDBC

--- src/com/nisum/trainReservation/UserJdbc.java
+++ src/com/nisum/trainReservation/UserJdbc.java
@@ -4,13 +4,35 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class UserJdbc {
-	Connection con=null;
-    try{  
-        Class.forName("com.mysql.jdbc.Driver");  
-        con=DriverManager.getConnection("jdbc:MysQL://localhost:3306/Train","root","root");  
-    }catch(Exception e){System.out.println(e);}  
-    return con;  
-}  
+	public static Connection getConnection(){  
+		Connection con=null;
+	    try{  
+	        Class.forName("com.mysql.jdbc.Driver");  
+	        con=DriverManager.getConnection("jdbc:MysQL://localhost:3306/Train","root","root");  
+	    }catch(Exception e){System.out.println(e);}
+	    return con;  
+	}  
+
+	public void createUser(User user) {
+		Connection con = getConnection();
+		  
+		String query = " insert into users (firstname, lastname, phone, email, password)"
+		+ " values (?, ?, ?, ?, ?)";
+		try {     
+		    PreparedStatement preparedStmt = con.prepareStatement(query);
+		    preparedStmt.setString (1, user.getFirstName());
+		    preparedStmt.setString (2, user.getLastName());
+		    preparedStmt.setDate   (3, user.getMobileNumber());
+		    preparedStmt.setBoolean(4, user.getEmail());
+		    preparedStmt.setInt    (5, user.getPassWord());
+		    preparedStmt.execute();
+		    con.close();
+	    }
+		catch (Exception e) {
+			System.err.println("Got an exception!");
+			System.err.println(e.getMessage());
+		}
+	}  
 
 
 }
--- src/com/nisum/trainReservation/UserRegistration.java
+++ src/com/nisum/trainReservation/UserRegistration.java
@@ -31,15 +31,15 @@ public class UserRegistration extends HttpServlet {
 		out.print("</head>");
 		out.println("<body>");
 		out.println("<label>Firstname</label>");
-		out.println("<input type=\"text\"><br><br>");
+		out.println("<input type=\"text\" name=firstname><br><br>");
 		out.println("<label>Lastrname</label>");
-		out.println("<input type=\"text\"><br><br>");
+		out.println("<input type=\"text\" name=lastname><br><br>");
 		out.println("<label>Mobilenumber</label>");
-		out.println("<input type=\"text\"><br><br>");
+		out.println("<input type=\"text\" name=phone><br><br>");
 		out.println("<label>Emailid</label>");
-		out.println("<input type=\"text\"><br><br>");
+		out.println("<input type=\"text\" name=email><br><br>");
 		out.println("<label>Password</label>");
-		out.println("<input type=\"text\"><br><br>");
+		out.println("<input type=\"text\" name=password><br><br>");
 		out.println("</body>");
 		out.println("</html>");
 		out.close();	
@@ -48,6 +48,16 @@ public class UserRegistration extends HttpServlet {
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String firstName = request.getParameter("firstname");
+		String lastName = request.getParameter("lastname");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String phone = request.getParameter("phone");
+		
+		UserService userService = new UserService(firstName, lastName, phone, email, password);
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.println("<h1>User Created Successfully!</h1>");
 
 	}
 
